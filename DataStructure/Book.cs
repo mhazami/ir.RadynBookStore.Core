ad189319ef7f350ecd1ed7564bbba07bf6e10c8f
Mohammad Azami <mohammadazami98@yahoo.com>
2019-09-16 00:37:33 +0430

    13980624

---
+++ DataStructure/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataStructure
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int FileId { get; set; }
+
+        public string Abstract { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ DataStructure/File.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataStructure
+{
+    public class File
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public byte[] Content { get; set; }
+
+        public string Extention { get; set; }
+    }
+}
